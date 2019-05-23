8418a3f0f803c4b3008f438312bbbe11040baa49
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-05-23 13:47:30 +0200

    Zwischenupdate script

--- script_diffusion_map.R
+++ script_diffusion_map.R
@@ -1,20 +1,60 @@
 ## diffusion map analysis of Hausgarten pikoplankton data
 
+rm(list=ls())
 # Zusammenfassung der Methode:
   
 # 1) Daten als Tabelle: Wir wollen eine Tabelle haben die die einzelnen Messungen als Zeilen enth?lt und die 
 # verschiedenen Parameter die gemessen werden als Zeilen. Der Methode ist es relativ egal wie diese Tabelle konstruiert wird. Im prinzip eignen sich sogar Bilddateien als eingabedaten, Der Farbwert es ersten pixels ist dann der erste Parameter, der Farbwert des zweiten pixels der zweite parameter und so weiter. Wenn ihr die Daten in anderer Form habt sollte das auch funktionieren.
-# 2) Standardtisierung: Wenn die Parameter vergleichbar sind, wie im Beispiel der Bild datei ist hier nicht zu tun, 
+
+# produce matrix to be replaced by data: -------------------
+
+set.seed(423)
+vec <- runif(440)
+
+mat <- matrix(vec, ncol=11)
+
+#-----------------------------------------------------------
+
+# 2) Standardisierung: Wenn die Parameter vergleichbar sind, wie im Beispiel der Bild datei ist hier nicht zu tun, 
 # wenn die Parameter sehr verschieden sind wie bei unserern Zensusdaten ist es gut die Spalten so zu normieren das jede 
 # spalte den Mittelwert 0 und Varianz 1 bekommt. 
+
+# standardize vector to mean = 0 and sd = 1 ----------------
+
+for(i in 1:ncol(mat)){
+  mat[, i] <- (mat[, i] - mean(mat[, i]))/sd(mat[, i]) 
+}
+
+# ----------------------------------------------------------
+
 # 3) Lokale Metrik: Jetzt definieren wir ein Mass f?r die ?hnlichkeit von zwei Zeilen D(x,y). Das kann zum Beispiel 
 # einfach der Euklidische  Abstand im Parameterraum sein. Aus dem Abstandsma? machen wir ein ?hnlichkeitsma? mit 
 # S(x,y)=1/D(x,y). Diese ?hnlichkeit wird f?r all Paare von Zeilen. F?r einen Datensatz mit N Zeilen erhalten wir 
 # so eine NxN Matrix von ?hnlichkeiten, S. 
+#~~ beinhaltet die Matrix dann nicht auch jeweils Abstände von Null weil auf ihrer Diagonalen die Zeilen mit sich selbst verglichen werden?
+
+
+# calculating euclidean distance for matrix mat ------------
+
+eucdists <- matrix(data = NA, nrow = nrow(mat), ncol = nrow(mat))
+
+for (i in 1:nrow(mat)){
+  for (j in 1:nrow(mat)){
+    eucdists[i,j] <- dist(rbind(mat[i, ], mat[j, ]), method = "euclidean")
+  }
+}
+
+# calculating degree of similarity--------------------------
+simil <- 1/eucdists
+
+
 # 4) Thresholding: Wir Vertrauen der ?hnlichkeit nur wenn sie hinreichend gro? ist. In einigen Systemen weiss man 
 # wie gro?, gro? genug ist. Es funktioniert aber auch die einfache regel "Eine ?hnlichkeit zwischen zwei Messungen 
 # ist signifikant wenn die ?hnlichkeit unter den 10 gr??ten ?hnlichkeiten f?r mindestens eine der Messungen ist". 
 # Alle zu kleinen/nicht signifikanten Eintr?ge in der S-Matrix werden null gesetzt.
+
+#~~ welche threshold wollen wir wählen?
+
 # 5) Laplace Matrix:  Aus der S-Matrix machen wir eine Laplace Matrix L. Die Formel dazu ist 
 # L[i,j]= - S[i,j] f?r i,j verschieden und  L[i,i] = sum_j S[i,j]. Die Originalmethode verwendet die normierte 
 # Laplcematrix, in der Zeile i nochmal durch L[i,i] geteilt wird. (Also L[i,i] wird 1) - kann man machen die einfache 
