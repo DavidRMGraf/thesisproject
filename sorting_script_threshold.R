0de8b4e72efa6d483075733da24f3ca5618c5b97
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-06-06 15:34:31 +0200

    WIP 06-06; 15:34

--- sorting_script_threshold.R
+++ sorting_script_threshold.R
@@ -1,22 +1,31 @@
 rm(list=ls())
 
+# any random matrix for functionality testing:
 set.seed(426)
 vec <- runif(440)
 mat <- matrix(vec, ncol=11)
 rm(vec)
 
-## 90%-threshold
+start.time <- Sys.time()
+
+#----
+# virtual dataframe is structured with each species in it's own column with location as rows
+
+## 90%-threshold--------------
 for (i in 1:ncol(mat)){
   mat[order(mat[, i], decreasing=T)[cumsum(mat[order(mat[, i], decreasing=T), i])/sum(mat[, i])>0.90], i] <- 0
 } 
-# 
-# ## 95%-threshold
+
+## 95%-threshold--------------
 # for (i in 1:ncol(mat)){
 #   mat[order(mat[, i], decreasing=T)[cumsum(mat[order(mat[, i], decreasing=T), i])/sum(mat[, i])>0.95], i] <- 0
 # }
-# 
-# ## 99%-threshold
+
+## 99%-threshold--------------
 # for (i in 1:ncol(mat)){
 #   mat[order(mat[, i], decreasing=T)[cumsum(mat[order(mat[, i], decreasing=T), i])/sum(mat[, i])>0.99], i] <- 0
 # }
 # 
+
+end.time <- Sys.time()
+end.time - start.time
