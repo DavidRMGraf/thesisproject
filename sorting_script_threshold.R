013b9f4966a065ca25e656484b51d04bacec7dca
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-06-06 15:27:49 +0200

    Wip 6-6 15:30

--- sorting_script_threshold.R
+++ sorting_script_threshold.R
@@ -5,6 +5,19 @@ vec <- runif(440)
 mat <- matrix(vec, ncol=11)
 rm(vec)
 
+## 90%-threshold
 for (i in 1:ncol(mat)){
-  mat[order(mat[, i], decreasing=T)[cumsum(mat[order(mat[, i], decreasing=T), i])/sum(mat[, i])>0.95], i] <- 0
+  mat[order(mat[, i], decreasing=T)[cumsum(mat[order(mat[, i], decreasing=T), i])/sum(mat[, i])>0.90], i] <- 0
 }
+# 
+# 
+# ## 95%-threshold
+# for (i in 1:ncol(mat)){
+#   mat[order(mat[, i], decreasing=T)[cumsum(mat[order(mat[, i], decreasing=T), i])/sum(mat[, i])>0.95], i] <- 0
+# }
+# 
+# ## 99%-threshold
+# for (i in 1:ncol(mat)){
+#   mat[order(mat[, i], decreasing=T)[cumsum(mat[order(mat[, i], decreasing=T), i])/sum(mat[, i])>0.99], i] <- 0
+# }
+# 
