02885fef7b0546635104e090abd2b030aac5d1ae
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-06-06 16:02:17 +0200

    WIP 06-06; 16:02

--- sorting_script_threshold.R
+++ sorting_script_threshold.R
@@ -9,10 +9,10 @@ rm(vec)
 # system time at start:
 start.time <- Sys.time()
 
-# description ----------------------------
+### description ----------------------------
 # virtual dataframe is structured with each species in it's own column with location as rows
 
-# per-species thresholds: ----------------
+### per-species thresholds: ----------------
 ## only keep entries if they are part of the lowest number of entries for each OTU that constitute 90/95/99 percent of their OTU
 
 ## 90%-threshold 
@@ -31,15 +31,22 @@ for (i in 1:ncol(rand.mat)){
 # }
 # 
 
-# total overall threshold: ---------------
+### total overall threshold: ---------------
 ## only keep OTUs that contribute at least 0.05% of all individuals in the analysis 
-
-# rand.mat <- rand.mat[, -colSums(rand.mat)/sum(rand.mat)<5e-04]
+# keep.cols <- colSums(rand.mat)/sum(rand.mat)>=5e-04
+# rand.mat <- rand.mat[, keep.cols]
 
 ## or 0.005%, respectively:
+# keep.cols <- colSums(rand.mat)/sum(rand.mat)>=5e-05
+# rand.mat <- rand.mat[, keep.cols]
 
-# rand.mat <- rand.mat[, -colSums(rand.mat)/sum(rand.mat)<5e-05]
+### sparse Matrix format to save space -----
+## to create 'sparse' matrices
 
-# system time at end and difference between start and end (RUNTIME) ------------
+# library(Matrix) 
+
+## http://www.johnmyleswhite.com/notebook/2011/10/31/using-sparse-matrices-in-r/
+
+### system time at end and difference between start and end (RUNTIME) ------------
 end.time <- Sys.time()
 end.time - start.time
