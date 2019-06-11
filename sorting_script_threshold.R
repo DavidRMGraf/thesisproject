6ec42af51d397479ca2edfcc8f89fa983fe6f9ca
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-06-11 10:23:37 +0200

    WIP 06-11; 10:23

--- sorting_script_threshold.R
+++ sorting_script_threshold.R
@@ -2,8 +2,8 @@ rm(list=ls())
 
 # any random matrix for functionality testing:
 set.seed(426)
-vec <- runif(440)
-rand.mat <- matrix(vec, ncol=11)
+vec <- runif(44000)
+rand.mat <- matrix(vec, ncol=110)
 rm(vec)
 
 # system time at start:
@@ -12,24 +12,23 @@ start.time <- Sys.time()
 ### description ----------------------------
 # virtual dataframe is structured with each species in it's own column with location as rows
 
-### per-species thresholds: ----------------
+### per-station species thresholds: ----------------
 ## only keep entries if they are part of the lowest number of entries for each OTU that constitute 90/95/99 percent of their OTU
 
 ## 90%-threshold 
-for (i in 1:ncol(rand.mat)){
-  rand.mat[order(rand.mat[, i], decreasing=T)[cumsum(rand.mat[order(rand.mat[, i], decreasing=T), i])/sum(rand.mat[, i])>0.90], i] <- 0
-} 
+for (i in 1:nrow(rand.mat)){
+  rand.mat[i, order(rand.mat[i, ], decreasing=T)[cumsum(rand.mat[i, order(rand.mat[i, ], decreasing=T)])/sum(rand.mat[i, ])>0.90]] <- 0
+}
 
 ## 95%-threshold
-# for (i in 1:ncol(rand.mat)){
-#   rand.mat[order(rand.mat[, i], decreasing=T)[cumsum(rand.mat[order(rand.mat[, i], decreasing=T), i])/sum(rand.mat[, i])>0.95], i] <- 0
+# for (i in 1:nrow(rand.mat)){
+#   rand.mat[i, order(rand.mat[i, ], decreasing=T)[cumsum(rand.mat[i, order(rand.mat[i, ], decreasing=T)])/sum(rand.mat[i, ])>0.95]] <- 0
 # }
 
 ## 99%-threshold
-# for (i in 1:ncol(rand.mat)){
-#   rand.mat[order(rand.mat[, i], decreasing=T)[cumsum(rand.mat[order(rand.mat[, i], decreasing=T), i])/sum(rand.mat[, i])>0.99], i] <- 0
+# for (i in 1:nrow(rand.mat)){
+#   rand.mat[i, order(rand.mat[i, ], decreasing=T)[cumsum(rand.mat[i, order(rand.mat[i, ], decreasing=T)])/sum(rand.mat[i, ])>0.99]] <- 0
 # }
-# 
 
 ### total overall threshold: ---------------
 ## only keep OTUs that contribute at least 0.05% of all individuals in the analysis 
@@ -50,3 +49,8 @@ for (i in 1:ncol(rand.mat)){
 ### system time at end and difference between start and end (RUNTIME) ------------
 end.time <- Sys.time()
 end.time - start.time
+
+## old (wrong) threshold code ----------------------------------------------------
+# for (i in 1:ncol(rand.mat)){
+#   rand.mat[order(rand.mat[, i], decreasing=T)[cumsum(rand.mat[order(rand.mat[, i], decreasing=T), i])/sum(rand.mat[, i])>0.90], i] <- 0
+# } 
