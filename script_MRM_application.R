6dd6abe72591c7d40993e0c9e26d966ed840395f
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-07-23 17:48:03 +0200

    WIP 07-23; 17:47

---
+++ script_MRM_application.R
@@ -0,0 +1,47 @@
+## script to perform the regressions on distance matrices
+rm(list=ls())
+graphics.off()
+
+#script -----------------------------------------------------------
+# get physical oceanography data
+phys_oce <- readRDS("physical_oceanography_data_all_years.rds")
+# get sequences
+sequ <- readRDS("sequences_thresh_applied.rds")
+
+# determine which columns to keep, based on physical dataset:
+col2keep <- complete.cases(phys_oce)
+#' here, we exclude the two 'vertippt' stations as they dont have
+#' coordinates!
+
+# reducing physical oceanography dataset:
+phys_oce <- phys_oce[col2keep,]
+range(phys_oce$year)
+unique(phys_oce$year)
+
+# reducing sequence dataset:
+sequ <- sequ[col2keep,]
+
+## physical distance matrix: --------------------------------------
+X <- phys_oce$longitude
+Y <- phys_oce$latitude
+points <- as.matrix(data.frame(X,Y))
+
+phys.dist.m <- geosphere::distm(points, fun = distVincentyEllipsoid)
+
+## temporal distance matrix: --------------------------------------
+temp.m  <- as.matrix(dist(phys_oce$year, method="euclidean", diag = FALSE, upper = FALSE))  
+
+## bray-curtis similarity matrix on sequences: --------------------
+bray.dis <- vegan::vegdist(sequ, method='bray')
+bray.sim <- 1-as.matrix(bray.dis)
+
+## MLM: -----------------------------------------------------------
+d <- list(bray.sim, phys.dist.m, temp.m)
+names(d) <- c('bray.sim','space','time')
+lower  <- lapply(d, ecodist::lower)
+d      <- as.data.frame(lapply(lower, c))
+
+
+m <- ecodist::MRM(bray.sim ~ space + time, data = d); m
+
+
