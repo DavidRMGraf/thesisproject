627114fcf71a020e2c6451b52819be1bd1f82656
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-08-05 10:27:58 +0200

    WIP 08-05; 10:27

--- script_supplements_univ_header.R
+++ script_supplements_univ_header.R
@@ -1,7 +1,6 @@
-# script diffusion map complete - 24.07.19
-#with otus, physical data (and optionally nutrients!)
-
-### this part was taken from the universal header! (START) ----
+# --------SCRIPT NAME HERE--------
+## universal header:
+### this part was taken from the universal header! (START) ~~~~~~~~~~~~~~~~~~~~~~~~
 rm(list = ls())
 graphics.off()
 
@@ -98,55 +97,4 @@ stat_names.phy <- stat_names[columns2keep.phy,]
 
 # remove unwanted filters, original datasets 
 rm(columns2keep.all, columns2keep.phy, sequ, stat_names, phys_oce)
-### this part was taken from the universal header! (END) ----
-
-## DM --------------------------------------------------------
-# variance-stabilizing transformation:
-
-f.n0.all <- zCompositions::cmultRepl(sequ.sub.all, method="CZM", label = 0)
-f.n0.phy <- zCompositions::cmultRepl(sequ.sub.phy, method="CZM", label = 0)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# physical data oder nutrients+physical data dazu?
-# f.n0.all <- cbind(f.n0.all, phys_oce.sub.all)
-# f.n0.phy <- cbind(f.n0.phy, phys_oce.sub.phy)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-f.clr.all <- CoDaSeq::codaSeq.clr(f.n0.all, samples.by.row = T)
-f.clr.phy <- CoDaSeq::codaSeq.clr(f.n0.phy, samples.by.row = T)
-
-# DM (Thilo's method)
-data.all <- similarity(as.matrix(f.n0.all))
-data.phy <- similarity(as.matrix(f.n0.phy))
-
-data.all <- simil_reducer(data.all)
-data.phy <- simil_reducer(data.phy)
-
-
-lap.all <- matrixLaplacian::matrixLaplacian(data.all, plot2D = F, plot3D = F)
-lap.phy <- matrixLaplacian::matrixLaplacian(data.phy, plot2D = F, plot3D = F)
-
-lap_mat.all <- lap.all$LaplacianMatrix
-lap_mat.phy <- lap.phy$LaplacianMatrix
-
-elm.all <- eigen(lap_mat.all)
-elm.phy <- eigen(lap_mat.phy)
-
-## map plot --------------------------------------------------
-library(rgdal)                                                                                                      
-library(raster)
-library(ggplot2)
-library(rworldxtra)
-library(rgeos)
-
-stat_coord.all <- stat_names.all[,c("latitude","longitude")] # select longitude and latitude columns
-#stat_coord <- na.omit(stat_coord.all) # remove missing values
-coordinates(stat_coord.all) <- ~longitude+latitude # spatial coordinate information 
-
-
-# get world map
-wm <- rworldmap::getMap(resolution ="high")
-wm <- crop(wm, extent(-20, 20, 76, 81)) # adjust extent of map to sampling region# simple plot
-
-plot(wm, axes=T) 
-points(coordinates(stat_coord.all)) 
+### this part was taken from the universal header! (END) ~~~~~~~~~~~~~~~~~~~~~~~~~~
