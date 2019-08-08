000de0bd20f07e146c528558cbedb7ca2b017f25
DavidRMGraf <david.ruben.max.graf@uni-oldenburg.de>
2019-08-08 12:33:28 +0200

    WIP 08-08; 12:30

--- script_dm_with_all_data.R
+++ script_dm_with_all_data.R
@@ -77,55 +77,70 @@ threshapply <- function(input_data, method){
 
 ## data
 # get physical oceanography data
-phys_oce <- readRDS("physical_oceanography_data_all_years.rds")
+phys_oce <- readRDS("physical_oceanography_data_with_ice.rds")
 
 # add 2 to the temperature data to make it compatible:
 phys_oce$temp_deg_c <- phys_oce$temp_deg_c+2
 
 #remove autocorrelated values from the physical oceanography dataset:
-phys_oce.sub.all <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit",
+phys_oce.sub.2014 <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit",
                                                 "NO3_mumol_l", "NO2_mumol_l", "SiOH4_mumol_l", "PO4_mumol_l"))
-phys_oce.sub.phy <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit"))
+phys_oce.sub.long <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit",
+                                                 "NO3_mumol_l", "NO2_mumol_l", "SiOH4_mumol_l", "PO4_mumol_l",  "icecover"))
+phys_oce.sub.2016 <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit", "icecover"))
 
 # cases need to be complete.cases AND the duplicates need to be excluded:
-columns2keep.all <- complete.cases(phys_oce.sub.all) & phys_oce$keep == 1  
-columns2keep.phy <- complete.cases(phys_oce.sub.phy) & phys_oce$keep == 1 
-columns2keep.2014 <- columns2keep.all & phys_oce$year == 2014
-columns2keep.HG <- columns2keep.all & phys_oce$latitude >= 78.5 & phys_oce$latitude <=80 & phys_oce$longitude >= -5 & phys_oce$longitude <= 11
-# borders of HG after: https://www.awi.de/en/science/biosciences/deep-sea-ecology-and-technology/observatories/lter-observatory-hausgarten.html
+# only values from 2014 in the whole Fram Strait:
+columns2keep.2014 <- complete.cases(phys_oce.sub.2014) & phys_oce$keep == 1 & phys_oce$year == 2014
+# all years except 2016, shallower than 30.1 m, in HAUSGARTEN:
+columns2keep.long <- complete.cases(phys_oce.sub.long) & phys_oce$keep == 1 & phys_oce$depth <= 30 & phys_oce$latitude >= 78.5 & phys_oce$latitude <=80 & phys_oce$longitude >= -5 & phys_oce$longitude <= 11
+# everything available from 2016:
+columns2keep.2016 <- complete.cases(phys_oce.sub.2016) & phys_oce$keep == 1 & phys_oce$year == 2016
+
+# columns2keep.HG <- columns2keep.all & phys_oce$latitude >= 78.5 & phys_oce$latitude <=80 & phys_oce$longitude >= -5 & phys_oce$longitude <= 11
+# # borders of HG after: https://www.awi.de/en/science/biosciences/deep-sea-ecology-and-technology/observatories/lter-observatory-hausgarten.html
 
 # reduce
-phys_oce.sub.2014 <- phys_oce.sub.all[columns2keep.2014,]
-phys_oce.sub.HG <- phys_oce.sub.all[columns2keep.HG,]
-phys_oce.sub.all <- phys_oce.sub.all[columns2keep.all,]
-phys_oce.sub.phy <- phys_oce.sub.phy[columns2keep.phy,]
+phys_oce.sub.2014 <- phys_oce.sub.2014[columns2keep.2014,]
+phys_oce.sub.long <- phys_oce.sub.long[columns2keep.long,]
+phys_oce.sub.2016 <- phys_oce.sub.2016[columns2keep.2016,]
+
+# phys_oce.sub.HG <- phys_oce.sub.all[columns2keep.HG,]
+# phys_oce.sub.all <- phys_oce.sub.all[columns2keep.all,]
+# phys_oce.sub.phy <- phys_oce.sub.phy[columns2keep.phy,]
 
 # get sequences
 sequ <- readRDS("sequ_all.rds")
-
-# apply 0.05 percent threshold
 sequ <- t(sequ)
+# apply 0.05 percent threshold
 sequ <- threshapply(sequ, "0.05 percent")
-
 dim(sequ)
 
-sequ.sub.all <- sequ[columns2keep.all,]
-sequ.sub.phy <- sequ[columns2keep.phy,]
 sequ.sub.2014 <- sequ[columns2keep.2014,]
-sequ.sub.HG <- sequ[columns2keep.HG,]
+sequ.sub.long <- sequ[columns2keep.long,]
+sequ.sub.2016 <- sequ[columns2keep.2016,]
+# sequ.sub.all <- sequ[columns2keep.all,]
+# sequ.sub.phy <- sequ[columns2keep.phy,]
+# sequ.sub.HG <- sequ[columns2keep.HG,]
 
 # get station names from the phys_oce datasheet:
 stat_names <- subset(phys_oce, select = c("Proben_ID_intern", "date", "depth",
                                           "year", "latitude", "longitude"))
+
 # reduce
-stat_names.all <- stat_names[columns2keep.all,]
-stat_names.phy <- stat_names[columns2keep.phy,]
 stat_names.2014 <- stat_names[columns2keep.2014,]
-stat_names.HG <- stat_names[columns2keep.HG,]
+stat_names.long <- stat_names[columns2keep.long,]
+stat_names.2016 <- stat_names[columns2keep.2016,]
+# stat_names.all <- stat_names[columns2keep.all,]
+# stat_names.phy <- stat_names[columns2keep.phy,]
+# stat_names.HG <- stat_names[columns2keep.HG,]
 
 ### this part was taken from the universal header! (END) ----
 # remove unwanted filters, original datasets 
-rm(columns2keep.all, columns2keep.phy, columns2keep.2014, columns2keep.HG, sequ, stat_names, phys_oce)
+rm(columns2keep.2014, columns2keep.2016, columns2keep.long,
+   sequ, stat_names, phys_oce,
+   threshapply)
+
 ## triggers:
 
 # calculate correlations?
@@ -133,69 +148,57 @@ calc.correl <- FALSE
 # plot worldmap?
 plot.wm <- FALSE
 
-
 ## DM --------------------------------------------------------
 
 # physical data oder nutrients+physical data dazu
-f.n0.all <- cbind(sequ.sub.all, phys_oce.sub.all)
-f.n0.phy <- cbind(sequ.sub.phy, phys_oce.sub.phy)
 f.n0.2014 <- cbind(sequ.sub.2014, phys_oce.sub.2014)
-f.n0.HG <- cbind(sequ.sub.HG, phys_oce.sub.HG)
+f.n0.long <- cbind(sequ.sub.long, phys_oce.sub.long)
+f.n0.2016 <- cbind(sequ.sub.2016, phys_oce.sub.2016)
+#f.n0.dummy <- cbind(sequ.sub.dummy, phys_oce.sub.dummy)
 
 # remove zeros
-f.n0.input.all <- zCompositions::cmultRepl(f.n0.all, method="CZM", label = 0)
-f.n0.input.phy <- zCompositions::cmultRepl(f.n0.phy, method="CZM", label = 0)
 f.n0.input.2014 <- zCompositions::cmultRepl(f.n0.2014, method="CZM", label = 0)
-f.n0.input.HG <- zCompositions::cmultRepl(f.n0.HG, method="CZM", label = 0)
+f.n0.input.long <- zCompositions::cmultRepl(f.n0.long, method="CZM", label = 0)
+f.n0.input.2016 <- zCompositions::cmultRepl(f.n0.2016, method="CZM", label = 0)
+#' for 2016, it produces a non-fatal warning that has no consequences for the output
+#' cause unkown.
+
+#f.n0.input.dummy <- zCompositions::cmultRepl(f.n0.dummy, method="CZM", label = 0)
 
 # variance-stabilizing transformation:
-f.clr.all <- CoDaSeq::codaSeq.clr(f.n0.input.all, samples.by.row = T)
-f.clr.phy <- CoDaSeq::codaSeq.clr(f.n0.input.phy, samples.by.row = T)
 f.clr.2014 <- CoDaSeq::codaSeq.clr(f.n0.input.2014, samples.by.row = T)
-f.clr.HG <- CoDaSeq::codaSeq.clr(f.n0.input.HG, samples.by.row = T)
+f.clr.long <- CoDaSeq::codaSeq.clr(f.n0.input.long, samples.by.row = T)
+f.clr.2016 <- CoDaSeq::codaSeq.clr(f.n0.input.2016, samples.by.row = T)
+#f.clr.dummy <- CoDaSeq::codaSeq.clr(f.n0.input.dummy, samples.by.row = T)
 
 # DM (Thilo's method)
-data.all <- similarity(as.matrix(f.clr.all))
-data.phy <- similarity(as.matrix(f.clr.phy))
 data.2014 <- similarity(as.matrix(f.clr.2014))
-data.HG <- similarity(as.matrix(f.clr.HG))
+data.long <- similarity(as.matrix(f.clr.long))
+data.2016 <- similarity(as.matrix(f.clr.2016))
+#data.dummy <- similarity(as.matrix(f.clr.dummy))
 
-data.all <- simil_reducer(data.all)
-data.phy <- simil_reducer(data.phy)
 data.2014 <- simil_reducer(data.2014)
-data.HG <- simil_reducer(data.HG)
+data.long <- simil_reducer(data.long)
+data.2016 <- simil_reducer(data.2016)
+#data.dummy <- simil_reducer(data.dummy)
 
-lap.all <- matrixLaplacian::matrixLaplacian(data.all, plot2D = F, plot3D = F)
-lap.phy <- matrixLaplacian::matrixLaplacian(data.phy, plot2D = F, plot3D = F)
 lap.2014 <- matrixLaplacian::matrixLaplacian(data.2014, plot2D = F, plot3D = F)
-lap.HG <- matrixLaplacian::matrixLaplacian(data.HG, plot2D = F, plot3D = F)
+lap.long <- matrixLaplacian::matrixLaplacian(data.long, plot2D = F, plot3D = F)
+lap.2016 <- matrixLaplacian::matrixLaplacian(data.2016, plot2D = F, plot3D = F)
+#lap.dummy <- matrixLaplacian::matrixLaplacian(data.dummy, plot2D = F, plot3D = F)
 
-lap_mat.all <- lap.all$LaplacianMatrix
-lap_mat.phy <- lap.phy$LaplacianMatrix
 lap_mat.2014 <- lap.2014$LaplacianMatrix
-lap_mat.HG <- lap.HG$LaplacianMatrix
+lap_mat.long <- lap.long$LaplacianMatrix
+lap_mat.2016 <- lap.2016$LaplacianMatrix
+#lap_mat.dummy <- lap.dummy$LaplacianMatrix
 
-elm.all <- eigen(lap_mat.all)
-elm.phy <- eigen(lap_mat.phy)
 elm.2014 <- eigen(lap_mat.2014)
-elm.HG <- eigen(lap_mat.HG)
+elm.long <- eigen(lap_mat.long)
+elm.2016 <- eigen(lap_mat.2016)
+#elm.dummy <- eigen(lap_mat.dummy)
 
 # construct data.frames from eigenvectors with lowest values from each elm result:
 
-low.all <- data.frame("minus_1" = elm.all$vectors[,ncol(data.all)-1], "minus_2" = elm.all$vectors[,ncol(data.all)-2],
-                      "minus_3" = elm.all$vectors[,ncol(data.all)-3], "minus_4" = elm.all$vectors[,ncol(data.all)-4],
-                      "minus_5" = elm.all$vectors[,ncol(data.all)-5], "minus_6" = elm.all$vectors[,ncol(data.all)-6],
-                      "minus_7" = elm.all$vectors[,ncol(data.all)-7], "minus_8" = elm.all$vectors[,ncol(data.all)-8],
-                      year = as.factor(stat_names.all$year),
-                      depth = -as.numeric(stat_names.all$depth),
-                      count = 1:ncol(data.all))
-low.phy <- data.frame("minus_1" = elm.phy$vectors[,ncol(data.phy)-1], "minus_2" = elm.phy$vectors[,ncol(data.phy)-2],
-                      "minus_3" = elm.phy$vectors[,ncol(data.phy)-3], "minus_4" = elm.phy$vectors[,ncol(data.phy)-4],
-                      "minus_5" = elm.phy$vectors[,ncol(data.phy)-5], "minus_6" = elm.phy$vectors[,ncol(data.phy)-6],
-                      "minus_7" = elm.phy$vectors[,ncol(data.phy)-7], "minus_8" = elm.phy$vectors[,ncol(data.phy)-8],
-                      year = as.factor(stat_names.phy$year),
-                      depth = -as.numeric(stat_names.phy$depth),
-                      count = 1:ncol(data.phy))
 low.2014 <- data.frame("minus_1" = elm.2014$vectors[,ncol(data.2014)-1], "minus_2" = elm.2014$vectors[,ncol(data.2014)-2],
                        "minus_3" = elm.2014$vectors[,ncol(data.2014)-3], "minus_4" = elm.2014$vectors[,ncol(data.2014)-4],
                        "minus_5" = elm.2014$vectors[,ncol(data.2014)-5], "minus_6" = elm.2014$vectors[,ncol(data.2014)-6],
@@ -203,19 +206,34 @@ low.2014 <- data.frame("minus_1" = elm.2014$vectors[,ncol(data.2014)-1], "minus_
                        year = as.factor(stat_names.2014$year),
                        depth = -as.numeric(stat_names.2014$depth),
                        count = 1:ncol(data.2014))
-low.HG <- data.frame("minus_1" = elm.HG$vectors[,ncol(data.HG)-1], "minus_2" = elm.HG$vectors[,ncol(data.HG)-2],
-                      "minus_3" = elm.HG$vectors[,ncol(data.HG)-3], "minus_4" = elm.HG$vectors[,ncol(data.HG)-4],
-                      "minus_5" = elm.HG$vectors[,ncol(data.HG)-5], "minus_6" = elm.HG$vectors[,ncol(data.HG)-6],
-                      "minus_7" = elm.HG$vectors[,ncol(data.HG)-7], "minus_8" = elm.HG$vectors[,ncol(data.HG)-8],
-                      year = as.factor(stat_names.HG$year),
-                      depth = -as.numeric(stat_names.HG$depth),
-                      count = 1:ncol(data.HG))
+low.long <- data.frame("minus_1" = elm.long$vectors[,ncol(data.long)-1], "minus_2" = elm.long$vectors[,ncol(data.long)-2],
+                      "minus_3" = elm.long$vectors[,ncol(data.long)-3], "minus_4" = elm.long$vectors[,ncol(data.long)-4],
+                      "minus_5" = elm.long$vectors[,ncol(data.long)-5], "minus_6" = elm.long$vectors[,ncol(data.long)-6],
+                      "minus_7" = elm.long$vectors[,ncol(data.long)-7], "minus_8" = elm.long$vectors[,ncol(data.long)-8],
+                      year = as.factor(stat_names.long$year),
+                      depth = -as.numeric(stat_names.long$depth),
+                      count = 1:ncol(data.long))
+low.2016 <- data.frame("minus_1" = elm.2016$vectors[,ncol(data.2016)-1], "minus_2" = elm.2016$vectors[,ncol(data.2016)-2],
+                      "minus_3" = elm.2016$vectors[,ncol(data.2016)-3], "minus_4" = elm.2016$vectors[,ncol(data.2016)-4],
+                      "minus_5" = elm.2016$vectors[,ncol(data.2016)-5], "minus_6" = elm.2016$vectors[,ncol(data.2016)-6],
+                      "minus_7" = elm.2016$vectors[,ncol(data.2016)-7], "minus_8" = elm.2016$vectors[,ncol(data.2016)-8],
+                      year = as.factor(stat_names.2016$year),
+                      depth = -as.numeric(stat_names.2016$depth),
+                      count = 1:ncol(data.2016))
+# low.dummy <- data.frame("minus_1" = elm.dummy$vectors[,ncol(data.dummy)-1], "minus_2" = elm.dummy$vectors[,ncol(data.dummy)-2],
+#                       "minus_3" = elm.dummy$vectors[,ncol(data.dummy)-3], "minus_4" = elm.dummy$vectors[,ncol(data.dummy)-4],
+#                       "minus_5" = elm.dummy$vectors[,ncol(data.dummy)-5], "minus_6" = elm.dummy$vectors[,ncol(data.dummy)-6],
+#                       "minus_7" = elm.dummy$vectors[,ncol(data.dummy)-7], "minus_8" = elm.dummy$vectors[,ncol(data.dummy)-8],
+#                       year = as.factor(stat_names.dummy$year),
+#                       depth = -as.numeric(stat_names.dummy$depth),
+#                       count = 1:ncol(data.dummy))
 
 ## correlations -------
 #' leaving out the first (smallest non-zero eigenvalue) and the corresponding eigenvector,
 #' each eigenvector is correlated with each column in the original dataset to find correlations
 #' between the eigenvectors and the predictors, determining what feature the eigenvector picks up
 
+# here, f.clr should be replaced with the untransformed values f.n0 as Katja confirmed!
 if (calc.correl){
   ## correlations 2014:
   cor.coef.2014 <- data.frame("eig.vec.ind" = sort(rep((length(elm.2014$values)-1):1, 10), decreasing = T),
@@ -234,23 +252,56 @@ if (calc.correl){
     cor.coef.2014$p.value[cor.coef.2014$eig.vec.ind == i] <- pval[logi]
   }
   
-  ## correlations HAUSGARTEN:
-  cor.coef.HG <- data.frame("eig.vec.ind" = sort(rep((length(elm.HG$values)-1):1, 10), decreasing = T),
-                              "order.ev" = rep(1:10, length(elm.HG$values)-1), "p-value" = NA,
+  ## correlations long:
+  cor.coef.long <- data.frame("eig.vec.ind" = sort(rep((length(elm.long$values)-1):1, 10), decreasing = T),
+                              "order.ev" = rep(1:10, length(elm.long$values)-1), "p-value" = NA,
                               "best.predictor" = NA, "cor.coeff" = NA)
   coeffic <- NA
   pval <- NA
-  for(i in (length(elm.HG$values)-1):1){
-    for(j in 1:dim(f.clr.HG)[2]){
-      coeffic[j] <- cor.test(elm.HG$vectors[, i], f.clr.HG[,j])[[4]]
-      pval[j] <- cor.test(elm.HG$vectors[, i], f.clr.HG[,j])[[3]]
+  for(i in (length(elm.long$values)-1):1){
+    for(j in 1:dim(f.clr.long)[2]){
+      coeffic[j] <- cor.test(elm.long$vectors[, i], f.clr.long[,j])[[4]]
+      pval[j] <- cor.test(elm.long$vectors[, i], f.clr.long[,j])[[3]]
     }
     logi <- order(abs(coeffic), decreasing = T)[1:10]
-    cor.coef.HG$best.predictor[cor.coef.HG$eig.vec.ind == i] <- paste("Var. ", colnames(f.clr.HG)[logi])
-    cor.coef.HG$cor.coeff[cor.coef.HG$eig.vec.ind == i] <- coeffic[logi]
-    cor.coef.HG$p.value[cor.coef.HG$eig.vec.ind == i] <- pval[logi]
+    cor.coef.long$best.predictor[cor.coef.long$eig.vec.ind == i] <- paste("Var. ", colnames(f.clr.long)[logi])
+    cor.coef.long$cor.coeff[cor.coef.long$eig.vec.ind == i] <- coeffic[logi]
+    cor.coef.long$p.value[cor.coef.long$eig.vec.ind == i] <- pval[logi]
   }
   
+  ## correlations 2016:
+  cor.coef.2016 <- data.frame("eig.vec.ind" = sort(rep((length(elm.2016$values)-1):1, 10), decreasing = T),
+                              "order.ev" = rep(1:10, length(elm.2016$values)-1), "p-value" = NA,
+                              "best.predictor" = NA, "cor.coeff" = NA)
+  coeffic <- NA
+  pval <- NA
+  for(i in (length(elm.2016$values)-1):1){
+    for(j in 1:dim(f.clr.2016)[2]){
+      coeffic[j] <- cor.test(elm.2016$vectors[, i], f.clr.2016[,j])[[4]]
+      pval[j] <- cor.test(elm.2016$vectors[, i], f.clr.2016[,j])[[3]]
+    }
+    logi <- order(abs(coeffic), decreasing = T)[1:10]
+    cor.coef.2016$best.predictor[cor.coef.2016$eig.vec.ind == i] <- paste("Var. ", colnames(f.clr.2016)[logi])
+    cor.coef.2016$cor.coeff[cor.coef.2016$eig.vec.ind == i] <- coeffic[logi]
+    cor.coef.2016$p.value[cor.coef.2016$eig.vec.ind == i] <- pval[logi]
+  }
+  
+  # ## correlations dummy:
+  # cor.coef.dummy <- data.frame("eig.vec.ind" = sort(rep((length(elm.dummy$values)-1):1, 10), decreasing = T),
+  #                             "order.ev" = rep(1:10, length(elm.dummy$values)-1), "p-value" = NA,
+  #                             "best.predictor" = NA, "cor.coeff" = NA)
+  # coeffic <- NA
+  # pval <- NA
+  # for(i in (length(elm.dummy$values)-1):1){
+  #   for(j in 1:dim(f.clr.dummy)[2]){
+  #     coeffic[j] <- cor.test(elm.dummy$vectors[, i], f.clr.dummy[,j])[[4]]
+  #     pval[j] <- cor.test(elm.dummy$vectors[, i], f.clr.dummy[,j])[[3]]
+  #   }
+  #   logi <- order(abs(coeffic), decreasing = T)[1:10]
+  #   cor.coef.dummy$best.predictor[cor.coef.dummy$eig.vec.ind == i] <- paste("Var. ", colnames(f.clr.dummy)[logi])
+  #   cor.coef.dummy$cor.coeff[cor.coef.dummy$eig.vec.ind == i] <- coeffic[logi]
+  #   cor.coef.dummy$p.value[cor.coef.dummy$eig.vec.ind == i] <- pval[logi]
+  # }
   
   cor.coef.HG[cor.coef.HG$order.ev == 1,]
 }
--- script_supplements_univ_header.R
+++ script_supplements_univ_header.R
@@ -1,6 +1,8 @@
-# --------SCRIPT NAME HERE--------
-## universal header:
-### this part was taken from the universal header! (START) ~~~~~~~~~~~~~~~~~~~~~~~~
+# script diffusion map complete - 24.07.19
+#with otus, physical data (and optionally nutrients!)
+
+### this part was taken from the universal header! (START) ----
+# MODIFIED
 rm(list = ls())
 graphics.off()
 
@@ -75,52 +77,68 @@ threshapply <- function(input_data, method){
 
 ## data
 # get physical oceanography data
-phys_oce <- readRDS("physical_oceanography_data_all_years.rds")
+phys_oce <- readRDS("physical_oceanography_data_with_ice.rds")
+
+# add 2 to the temperature data to make it compatible:
+phys_oce$temp_deg_c <- phys_oce$temp_deg_c+2
 
 #remove autocorrelated values from the physical oceanography dataset:
 phys_oce.sub.all <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit",
-                                                "NO3_mumol_l", "NO2_mumol_l", "SiOH4_mumol_l", "PO4_mumol_l"))
-phys_oce.sub.phy <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit"))
+                                                "NO3_mumol_l", "NO2_mumol_l", "SiOH4_mumol_l", "PO4_mumol_l", "icecover"))
+phys_oce.sub.phy <- subset(phys_oce, select = c("depth", "temp_deg_c", "salinity", "flurom_arbit", "icecover"))
 
 # cases need to be complete.cases AND the duplicates need to be excluded:
-columns2keep.all <- complete.cases(phys_oce.sub.all) & phys_oce$keep == 1  
+columns2keep.all <- complete.cases(phys_oce.sub.all) & phys_oce$keep == 1
 columns2keep.phy <- complete.cases(phys_oce.sub.phy) & phys_oce$keep == 1 
+
 columns2keep.2014 <- columns2keep.all & phys_oce$year == 2014
-columns2keep.HG <- columns2keep.all & phys_oce$latitude >= 78.5 & phys_oce$latitude <=80 & phys_oce$longitude >= -5 & phys_oce$longitude <= 11
-# borders of HG after: https://www.awi.de/en/science/biosciences/deep-sea-ecology-and-technology/observatories/lter-observatory-hausgarten.html
+columns2keep.long <- columns2keep.all & phys_oce$depth <= 30
+columns2keep.2016 <- columns2keep.phy & phys_oce$year == 2016
+
+# columns2keep.HG <- columns2keep.all & phys_oce$latitude >= 78.5 & phys_oce$latitude <=80 & phys_oce$longitude >= -5 & phys_oce$longitude <= 11
+# # borders of HG after: https://www.awi.de/en/science/biosciences/deep-sea-ecology-and-technology/observatories/lter-observatory-hausgarten.html
 
 # reduce
-#' one change of order here as phys_oce.sub.all is overwritten in the third row
 phys_oce.sub.2014 <- phys_oce.sub.all[columns2keep.2014,]
-phys_oce.sub.HG <- phys_oce.sub.all[columns2keep.HG,]
-phys_oce.sub.all <- phys_oce.sub.all[columns2keep.all,]
-phys_oce.sub.phy <- phys_oce.sub.phy[columns2keep.phy,]
+phys_oce.sub.long <- phys_oce.sub.all[columns2keep.long,]
+phys_oce.sub.2016 <- phys_oce.sub.phy[columns2keep.2016,]
+
+# phys_oce.sub.HG <- phys_oce.sub.all[columns2keep.HG,]
+# phys_oce.sub.all <- phys_oce.sub.all[columns2keep.all,]
+# phys_oce.sub.phy <- phys_oce.sub.phy[columns2keep.phy,]
 
 # get sequences
 sequ <- readRDS("sequ_all.rds")
-
-# apply 0.05 percent threshold
 sequ <- t(sequ)
-sequ <- threshapply(sequ, "0.05 percent")
 
-# check dimensions
 dim(sequ)
 
-# subset sequences file:
-sequ.sub.all <- sequ[columns2keep.all,]
-sequ.sub.phy <- sequ[columns2keep.phy,]
 sequ.sub.2014 <- sequ[columns2keep.2014,]
-sequ.sub.HG <- sequ[columns2keep.HG,]
+sequ.sub.long <- sequ[columns2keep.long,]
+sequ.sub.2016 <- sequ[columns2keep.2016,]
+# sequ.sub.all <- sequ[columns2keep.all,]
+# sequ.sub.phy <- sequ[columns2keep.phy,]
+# sequ.sub.HG <- sequ[columns2keep.HG,]
+
+# apply 0.05 percent threshold
+sequ.sub.2014 <- threshapply(sequ.sub.2014, "0.05 percent")
+sequ.sub.long <- threshapply(sequ.sub.long, "0.05 percent")
+sequ.sub.2016 <- threshapply(sequ.sub.2016, "0.05 percent")
+# sequ.sub.all <- threshapply(sequ.sub.all, "0.05 percent")
+# sequ.sub.phy <- threshapply(sequ.sub.phy, "0.05 percent")
+# sequ.sub.HG <- threshapply(sequ.sub.HG, "0.05 percent")
+
+
 
 # get station names from the phys_oce datasheet:
 stat_names <- subset(phys_oce, select = c("Proben_ID_intern", "date", "depth",
                                           "year", "latitude", "longitude"))
-# subset stat_names
-stat_names.all <- stat_names[columns2keep.all,]
-stat_names.phy <- stat_names[columns2keep.phy,]
+# reduce
 stat_names.2014 <- stat_names[columns2keep.2014,]
-stat_names.HG <- stat_names[columns2keep.HG,]
+stat_names.long <- stat_names[columns2keep.long,]
+stat_names.2016 <- stat_names[columns2keep.2016,]
+# stat_names.all <- stat_names[columns2keep.all,]
+# stat_names.phy <- stat_names[columns2keep.phy,]
+# stat_names.HG <- stat_names[columns2keep.HG,]
 
-# remove unwanted filters, original datasets 
-rm(columns2keep.all, columns2keep.phy, columns2keep.2014, columns2keep.HG, sequ, stat_names, phys_oce)
-### this part was taken from the universal header! (END) ~~~~~~~~~~~~~~~~~~~~~~~~~~
+### this part was taken from the universal header! (END) ----
